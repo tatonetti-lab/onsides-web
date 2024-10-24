e54d7dd28475237b472cada4ba57914aeb2f48fa
zietzm <michael.zietz@gmail.com>
2024-10-24 12:20:39 -0700

    feat: add gunicorn production server

--- backend/serve_pages.py
+++ backend/serve_pages.py
@@ -18,7 +18,6 @@ list_of_build_files = set(os.listdir("../frontend/build"))
 @serve_pages.route("/", defaults={"path": ""})
 @serve_pages.route("/<path:path>")
 def catch_all(path):
-    # print(path)
     if path in list_of_build_files:
         return send_from_directory("../frontend/build", path)
     return render_template("index.html")
