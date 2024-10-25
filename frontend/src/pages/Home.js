50d8530841d07df4bda8b513fab9fb47fe6dbe39
zietzm <michael.zietz@gmail.com>
2024-10-25 11:26:33 -0700

    feat: update home page description

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -31,36 +31,47 @@ export default function Home() {
         <br />
 
         <p>
-          {" "}
-          Initial release of the Onsides database of adverse reactions and boxed
-          warnings extracted from the FDA structured product labels. All labels
-          available to download from{" "}
-          <a
-            href="https://dailymed.nlm.nih.gov/dailymed/spl-resources-all-drug-labels.cfm"
-            target="_blank"
-            rel="noreferrer"
-          >
-            DailyMed
+          OnSIDES is a database of adverse drug events extracted from drug
+          labels created by fine-tuning a{" "}
+          <a href="https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract">
+            PubMedBERT language model
           </a>{" "}
-          as of April 2022 were processed in this analysis. In total 2.7 million
-          adverse reactions were extracted from 42,000 labels for just under
-          2,000 drug ingredients or combination of ingredients.
-        </p>
-
-        <p>
-          Onsides was created using the ClinicalBERT language model and 200
-          manually curated labels available from{" "}
-          <a
-            href="https://pubmed.ncbi.nlm.nih.gov/29381145/"
-            target="_blank"
-            rel="noreferrer"
-          >
+          on 200 manually curated labels available from{" "}
+          <a href="https://pubmed.ncbi.nlm.nih.gov/29381145/">
             Denmer-Fushman et al.
           </a>
-          . The model achieves an F1 score of 0.86, AUROC of 0.88, and AUPR of
-          0.91 at extracting effects from the ADVERSE REACTIONS section of the
-          label and an F1 score of 0.66, AUROC of 0.71, and AUPR of 0.60 at
-          extracting effects from the BOXED WARNINGS section.{" "}
+          . This comprehensive database will be updated quarterly, and currently
+          contains more than 3.6 million drug-ADE pairs for 2,793 drug
+          ingredients extracted from 46,686 labels, processed from all of the
+          labels available to download from{" "}
+          <a href="https://dailymed.nlm.nih.gov/dailymed/spl-resources-all-drug-labels.cfm">
+            DailyMed
+          </a>{" "}
+          as of November 2023. Additionally, we now provide a number of
+          complementary databases constructed using a similar method -
+          OnSIDES-INTL, adverse drug events extracted from drug labels of other
+          nations/regions (Japan, UK, EU), and OnSIDES-PED, adverse drug events
+          specifically noted for pediatric patients in drug labels. We have
+          recently released a preprint on{" "}
+          <a href="https://www.medrxiv.org/content/10.1101/2024.03.22.24304724v1">
+            medRxiv
+          </a>{" "}
+          with a full description of the data, methods and analyses.
+        </p>
+        <h3 id="model-accuracy">Model Accuracy</h3>
+        <p>
+          Our fine-tuned language model achieves an F1 score of 0.90, AUROC of
+          0.92, and AUPR of 0.95 at extracting effects from the ADVERSE
+          REACTIONS section of the FDA drug label. For the BOXED WARNINGS
+          section, the model achieves an F1 score of 0.71, AUROC of 0.85, and
+          AUPR of 0.72. For the WARNINGS AND PRECUATIONS section, the model
+          achieves an F1 score of 0.68, AUROC of 0.66, and AUPR of 0.68.
+          Compared against the reference standard using the official evaluation
+          script for{" "}
+          <a href="https://bionlp.nlm.nih.gov/tac2017adversereactions/">
+            TAC 2017
+          </a>
+          , the model achieves a Micro-F1 score of 0.87 and a Macro-F1 of 0.85.
         </p>
       </Container>
     </div>
