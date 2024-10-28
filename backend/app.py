bfad8f297c7fe15afb4f818c533797fa012b33f5
zietzm <michael.zietz@gmail.com>
2024-10-28 12:17:57 -0700

    fix: cors should be commented on deploy

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask_cors import CORS  # comment this on deployment
 
+# from flask_cors import CORS  # comment this on deployment
 from api import api
 
 app = Flask(
     __name__,
 )
 
-CORS(app, resources=["/api/*"], origins=["*"])  # comment this on deployment
+# CORS(app, resources=["/api/*"], origins=["*"])  # comment this on deployment
 
 
 app.register_blueprint(api)
